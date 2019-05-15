dd409bbe79a4779bac294296609a220d535dbb33
princeYang <lepetitprince517@sjtu.edu.cn>
2019-05-15 13:55:37 +0800

    optiming

--- src/princeYang/mxcc/Config.java
+++ src/princeYang/mxcc/Config.java
@@ -3,7 +3,7 @@ package princeYang.mxcc;
 public class Config
 {
     static public final int regSize = 8;
-    static public final int INLINE_MAX_INST = 256;
-    static public final int INLINE_FUNC_INST = 6000;
+    static public final int INLINE_MAX_INST = 512;
+    static public final int INLINE_FUNC_INST = 8192;
     static public final int INLINE_MAX_DEPTH = 8;
 }
